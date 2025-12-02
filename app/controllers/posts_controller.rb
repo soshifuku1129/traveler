d4253be7ba9cf6c70bc081f34d339ff5d76c492a
soshifuku1129 <231849242+soshifuku1129@users.noreply.github.com>
2025-12-02 23:03:02 +0900

    編集機能から

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
 
   def index
